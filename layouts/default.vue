8f23927ba0bb8e5ba5726e5b8c58fd84bd763e82
Torgovtsev Vladislav <vlad4990@gmail.com>
2020-12-06 21:21:40 +0300

    Рабочий каталог курсов и удаление из стейта

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <navbar />
+
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/v-navbar'
+export default {
+  components:{
+    navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
