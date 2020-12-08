1bf90bf26b4b7d82e2743c09d821fd3e0fe56750
Torgovtsev Vladislav <vlad4990@gmail.com>
2020-12-08 22:43:36 +0300

    add dist to gh-pages

---
+++ dist/_nuxt/static/1607456577/courses/add/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/courses/add", {data:[{}],fetch:[],mutations:[]});
---
+++ dist/_nuxt/static/1607456577/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcourses\u002Fadd","\u002Fcourses","\u002Fabout"]})
